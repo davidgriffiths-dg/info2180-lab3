a44a7cac3867d0f1788a0f373c30a0b5591c32fd
David <griffithsdavidchris@gmail.com>
2020-10-13 21:40:17 -0500

    Fixed game winner code

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -18,6 +18,7 @@ var winning_combinations = [
   var player1 = 1;
   var p1array = [];
   var p2array = [];
+  console.log("X plays first")
 
     for(let i=0;i<cells.length;i++){
     cells[i].classList.add("square");
@@ -31,36 +32,48 @@ var winning_combinations = [
     function startGame(){
 
         if (player1 == 1) {
-            cells[i].textContent = "X";
+            cells[i].innerHTML = "X";
             cells[i].classList.add("X");
             player1 = 0;
             p1array.push(i);
             cells[i].onclick = false;
 
-            if (p1array.includes(winning_combinations[0]  || winning_combinations[1] || winning_combinations[2] || winning_combinations[2] ||
-                winning_combinations[4] ||winning_combinations[5] || winning_combinations[6] ||winning_combinations[7]||winning_combinations[8])) {
+            if (p1array.includes(0) &&  p1array.includes(1) && p1array.includes(1) ||
+                p1array.includes(3) &&  p1array.includes(4) && p1array.includes(5) || 
+                p1array.includes(6) &&  p1array.includes(7) && p1array.includes(8) ||
+                p1array.includes(0) &&  p1array.includes(3) && p1array.includes(6) ||
+                p1array.includes(1) &&  p1array.includes(4) && p1array.includes(7) ||
+                p1array.includes(2) &&  p1array.includes(5) && p1array.includes(8) ||
+                p1array.includes(0) &&  p1array.includes(4) && p1array.includes(8) ||
+                p1array.includes(2) &&  p1array.includes(4) && p1array.includes(6)){
                 document.getElementById("status").classList.add("you-won");
-                document.getElementById("status").textContent = "Congratulations! X is the Winner!";
+                document.getElementById("status").innerHTML = "Congratulations! X is the Winner!";
             }
 
-        } else{
-            cells[i].textContent = "O";
+        } else  {
+            cells[i].innerHTML = "O";
             cells[i].classList.add("O");
             player1 = 1;
             p2array.push(i);
             cells[i].onclick = false;
 
-            if (p1array.includes(winning_combinations[0]  || winning_combinations[1] || winning_combinations[2] || winning_combinations[2] ||
-                winning_combinations[4] ||winning_combinations[5] || winning_combinations[6] ||winning_combinations[7]||winning_combinations[8])) {
+            if (p2array.includes(0) &&  p2array.includes(1) && p2array.includes(1) ||
+                p2array.includes(3) &&  p2array.includes(4) && p2array.includes(5) || 
+                p2array.includes(6) &&  p2array.includes(7) && p2array.includes(8) ||
+                p2array.includes(0) &&  p2array.includes(3) && p2array.includes(6) ||
+                p2array.includes(1) &&  p2array.includes(4) && p2array.includes(7) ||
+                p2array.includes(2) &&  p2array.includes(5) && p2array.includes(8) ||
+                p2array.includes(0) &&  p2array.includes(4) && p2array.includes(8) ||
+                p2array.includes(2) &&  p2array.includes(4) && p2array.includes(6)){
                 document.getElementById("status").classList.add("you-won");
-                document.getElementById("status").textContent = "Congratulations! O is the Winner!";
+                document.getElementById("status").innerHTML = "Congratulations! O is the Winner!";
         }
 
   }
 }
 const restartGame = document.getElementsByClassName("btn");
 restartGame[0].addEventListener('click', function() {
-    cells[i].textContent = "";
+    cells[i].innerHTML = "";
     gameFunction();
 });
 
